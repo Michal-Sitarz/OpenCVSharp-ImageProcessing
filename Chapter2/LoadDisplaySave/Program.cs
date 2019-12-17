fa9dbd48b6d1b63d1d876bd08ee27355366d096f
Michael Sitarz <michael.sitarz@doverfs.com>
2019-12-17 15:56:44 +0000

    Load, Display, Save image - fully working

---
+++ Chapter2/LoadDisplaySave/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace LoadDisplaySave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null)
+            {
+                Console.WriteLine("No arguments passed :/");
+            }
+            else
+            {
+                string filename = args[0];
+                Console.WriteLine(filename);
+
+                Mat image = Cv2.ImRead(filename, ImreadModes.Grayscale);
+
+                Cv2.ImShow("leaf", image);
+
+                Cv2.ImWrite(@"../../images/leafSaved.jpg", image);
+
+                Cv2.WaitKey();
+                Cv2.DestroyAllWindows();
+            }
+        }
+    }
+}
