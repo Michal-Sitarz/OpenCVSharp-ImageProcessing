46240f321683e3e5d90513b5f4791dd167c956d5
Michal-Sitarz <me@michalsitarz.com>
2020-01-10 12:08:58 +0000

    Image Processing - Basic Operations

---
+++ Chapter5/BitwiseOperationsOnImages/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace BitwiseOperationsOnImages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // BitwiseAND
+            // BitwiseOR
+            // BitwiseXOR
+            // BitwiseNOT
+
+            Mat image1 = Mat.Zeros(new Size(400, 200), MatType.CV_8UC1);
+            Mat image2 = Mat.Zeros(new Size(400, 200), MatType.CV_8UC1);
+
+            Cv2.Rectangle(image1, new Rect(new Point(0, 0), new Size(image1.Cols / 2, image1.Rows)), new Scalar(255, 255, 255), -1);
+            Cv2.ImShow("image1", image1);
+
+            Cv2.Rectangle(image2, new Rect(new Point(150, 100), new Size(200,50)), new Scalar(255, 255, 255), -1);
+            Cv2.ImShow("image2", image2);
+
+            Mat andImage = new Mat();
+            Cv2.BitwiseAnd(image1, image2, andImage);
+            Cv2.ImShow("andImage", andImage);
+
+            Mat orImage = new Mat();
+            Cv2.BitwiseOr(image1, image2, orImage);
+            Cv2.ImShow("orImage", orImage);
+
+            Mat xorImage = new Mat();
+            Cv2.BitwiseXor(image1, image2, xorImage);
+            Cv2.ImShow("xorImage", xorImage);
+            
+            Mat notImage1 = new Mat();
+            Cv2.BitwiseNot(image1, notImage1);
+            Cv2.ImShow("notImage1", notImage1);
+
+            Mat notImage2 = new Mat();
+            Cv2.BitwiseNot(image2, notImage2);
+            Cv2.ImShow("notImage2", notImage2);
+
+
+            Cv2.WaitKey();
+            Cv2.DestroyAllWindows();
+        }
+    }
+}
--- Chapter5/FirstImageProcessingSteps/Program.cs
+++ Chapter5/FirstImageProcessingSteps/Program.cs
@@ -8,12 +8,55 @@ namespace FirstImageProcessingSteps
         static void Main(string[] args)
         {
             Mat image = Cv2.ImRead(@"../../images/paradise.jpg", ImreadModes.Color);
+            Cv2.Resize(image, image, new Size(image.Width / 1.5, image.Height / 1.5));
+            Mat dest = new Mat();
 
+            // SHIFTING/TRANSLATING
+            /*
+            // Translation Matrix
+            // [1,0,tx]
+            // [0,1,ty]
+            float[] data = { 1, 0, 100, 0, 1, 100 };
+            Mat M = new Mat(2, 3, MatType.CV_32FC1, data);
+            
+            Cv2.WarpAffine(image, dest, M, new Size(image.Width + 100, image.Height + 100));
+            */
 
+            // ROTATING
+            /*
+            var center = new Point2f(image.Width / 2, image.Height / 2);
+            double angle = -45.0;
+            Mat RM = Cv2.GetRotationMatrix2D(center, angle, 0.5);
+                        
+            Cv2.WarpAffine(image, dest, RM, new Size(image.Width, image.Height));
+            */
 
+            // RESIZING
+            /*
+            Cv2.Resize(image, dest, new Size(image.Width / 2, image.Height / 2));
 
+            Mat dest2 = new Mat();
+            Cv2.Resize(image, dest2, new Size(0, 0), 0.75, 0.5);
+            */
+
+            // FLIPPING
+            
+            Mat dst1 = new Mat();
+            Mat dst2 = new Mat();
+            Mat dst3 = new Mat();
+
+            Cv2.Flip(image, dst1, FlipMode.X); // flip around x-axis (horizontal axis) <- called "vertical flipping"
+            Cv2.Flip(image, dst2, FlipMode.Y); // flip around y-axis (vertical axis) <- called "horizontal flipping"
+            Cv2.Flip(image, dst3, FlipMode.XY); // flip around both axis, first X mode, then Y mode = same as 180 degree rotation
+
+            Cv2.ImShow("dst1", dst1);
+            Cv2.ImShow("dst2", dst2);
+            Cv2.ImShow("dst3", dst3);
+            
+                       
 
             Cv2.ImShow("original", image);
+            //Cv2.ImShow("processed", dest);
 
             Cv2.WaitKey();
             Cv2.DestroyAllWindows();
